a86290852295734c22bf6fbc9fa80320b1816f5a
pablo <pablo@192.168.1.39>
2023-05-11 12:27:30 +0200

    este es la actualizacion

--- pro1/src/ej1/principal.java
+++ pro1/src/ej1/principal.java
@@ -5,6 +5,7 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ej de repaso.");
+		System.out.println("Actualizacion");
 
 	}
 
